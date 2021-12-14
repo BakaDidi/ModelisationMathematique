7b8d1e37b54be02f7c4867a8a80a05e2b9ae7416
Loic Récoupé <loic.recoupe@gmail.com>
2021-12-14 10:41:51 +0100

    edit

--- stats.py
+++ stats.py
@@ -1,30 +1,33 @@
 from numpy import loadtxt
+import numpy as np
 import random
-
-
-
+import pandas
+import matplotlib.pyplot as plt
 
 names = []
 
-with open("eggs.csv", 'rb') as file:
+filename = input("Filename:")
+
+with open(filename, 'rb') as file:
     probas = loadtxt(file, delimiter=';')
 
 for i in range(len(probas)):
 
     names.append(chr(65+i))
 
-# nbLances = int(input("Nombre de lancers: "))
-# nbDeplacements = int(input("Nombre de déplacements autorisés: "))
+nbLances = int(input("Nombre de lancers: "))
+nbDeplacements = int(input("Nombre de déplacements autorisés: "))
 
-nbLances = 10
-nbDeplacements=2
+# nbLances = 10
+# nbDeplacements=5
 
+total = []
 
 
 for i in range(nbLances):
 
     pos = 0 # position = A
-
+    posHistory = ["A"]
     for j in range(nbDeplacements):
 
         randomNb = random.random()
@@ -32,20 +35,69 @@ for i in range(nbLances):
         unDone = True
 
         infBorne = 0 # valeur
-        supBorne = 0 # index
+        supBorneIndex = 0
+        supBorne = probas[pos][supBorneIndex]
 
         resultIndex = None
 
         while unDone:
-            if infBorne < randomNb and randomNb < probas[pos][supBorne]:
-                resultIndex = supBorne
+            if infBorne < randomNb and randomNb < supBorne:
+                resultIndex = supBorneIndex
                 unDone = False
             else:
-                infBorne += probas[pos][supBorne]
-                supBorne += 1
+                infBorne += probas[pos][supBorneIndex]
+                supBorneIndex += 1
+                supBorne += probas[pos][supBorneIndex]
 
 
-        print(resultIndex)
+        pos = resultIndex
+        posHistory.append(names[pos])
+
+    total.append(posHistory)
+
+
+totalFreqs = []
+
+
+totalProba = pandas.DataFrame.from_records(data=total)
+# print(total)
+
+for column in totalProba.iteritems():
+    freqs = []
+    for name in names:
+        freqs.append(column[1].values.tolist().count(name) / nbLances)
+    totalFreqs.append(freqs)
+
+print("\n[ A,   B,   C ]")
+
+totalFreqs = pandas.DataFrame.from_records(columns=names, data=totalFreqs)
+
+freqsAffichage = []
+
+for freq in totalFreqs.iteritems():
+    freqsAffichage.append(freq[1].values.tolist())
+
+
+
+
+freqsAffichage = np.asarray(freqsAffichage)
+i = 0
+for line in freqsAffichage:
+    plt.plot(line, label=chr(65+i))
+    plt.legend()
+    i+=1
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
 
 
 
